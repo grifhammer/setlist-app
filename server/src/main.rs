9d5e46f9b356ba34de26930fcef5127f9ca3d4eb
Griffin Hammer <grifhammer@gmail.com>
2020-02-28 13:07:51 -0800

    Fixed Cors issue

--- server/src/main.rs
+++ server/src/main.rs
@@ -3,8 +3,11 @@
 #[macro_use]
 extern crate rocket;
 extern crate rocket_contrib;
+extern crate rocket_cors;
 
+use rocket::http::Method;
 use rocket_contrib::json::*;
+use rocket_cors::{AllowedHeaders, AllowedOrigins, Error};
 use setlist_app::fns::show_setlists;
 use setlist_app::models::*;
 use setlist_app::resources::db::PostgresConn;
@@ -14,9 +17,20 @@ fn index(connection: PostgresConn) -> Json<Vec<Setlist>> {
     Json(show_setlists(connection))
 }
 
-fn main() {
+fn main() -> Result<(), Error> {
+    let allowed_origins = AllowedOrigins::all();
+    let cors = rocket_cors::CorsOptions {
+        allowed_origins,
+        allowed_methods: vec![Method::Get].into_iter().map(From::from).collect(),
+        allowed_headers: AllowedHeaders::some(&["Authorization", "Accept"]),
+        allow_credentials: true,
+        ..Default::default()
+    }
+    .to_cors()?;
     rocket::ignite()
         .mount("/", routes![index])
+        .attach(cors)
         .attach(PostgresConn::fairing())
         .launch();
+    Ok(())
 }
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,26 +1,40 @@
-import * as React from 'react';
-import './App.css';
+import * as React from "react";
+import "./App.css";
 
-import logo from './logo.svg';
+import logo from "./logo.svg";
 
 class App extends React.Component<{}, { apiMessage: string }> {
   constructor(props: object) {
     super(props);
 
-    this.state = { apiMessage: "Loading... (If this takes too long, the database might be down.)" };
+    this.state = {
+      apiMessage:
+        "Loading... (If this takes too long, the database might be down.)"
+    };
+
+    this.handleChange = this.handleChange.bind(this);
   }
 
   public componentDidMount() {
-    fetch("/api")
-      .then(r => r.status === 500
-        ? `(The server reported an error or cannot be reached. Is it compiling...?)`
-        : r.text()
+    fetch("http://localhost:8000/api")
+      .then(r =>
+        r.status === 500
+          ? `(The server reported an error or cannot be reached. Is it compiling...?)`
+          : r.text()
       )
       .then(apiMessage =>
         this.setState({
           apiMessage
         })
       );
+  }
+
+  public handleChange(event: React.FormEvent<HTMLFormElement>) {
+    this.setState({ apiMessage: `${event.timeStamp}` });
+  }
+
+  public searchArtist(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
   }
 
   public render() {
@@ -33,9 +47,14 @@ class App extends React.Component<{}, { apiMessage: string }> {
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <p>
-          {this.state.apiMessage}
-        </p>
+        <form onSubmit={this.searchArtist} onChange={this.handleChange}>
+          <label>
+            Artist:
+            <input type="text" name="name" />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        <p>{this.state.apiMessage}</p>
       </div>
     );
   }
