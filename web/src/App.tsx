850ee3daa6b306502106c07f7e01ca0ae0844f90
Griffin Hammer <grifhammer@gmail.com>
2021-01-24 23:25:11 -0800

    Remove Rest of Router

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { Route, Switch } from "react-router-dom";
 import Home from "./Home";
 import Login from "./Login";
 
@@ -12,10 +11,10 @@ class App extends React.Component<{}, {}> {
 
   public render() {
     return (
-      <Switch>
-        <Route path="/login" component={Login} />
-        <Route exact={true} path="/" component={Home} />
-      </Switch>
+      <React.Fragment>
+        <Login />
+        <Home />
+      </React.Fragment>
     );
   }
 }
