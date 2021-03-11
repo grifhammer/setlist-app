b59ffbe8e776a4147c31ec43d6a434ce3542cdbc
Griffin Hammer <grifhammer@gmail.com>
2021-03-11 14:33:02 -0800

    f

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -12,7 +12,7 @@ class App extends React.Component<{}, {}> {
   public render() {
     return (
       <React.Fragment>
-        Fuck
+        F
         <Login />
         <Home />
       </React.Fragment>
