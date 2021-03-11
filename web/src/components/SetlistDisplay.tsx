f5bd1730f57f21c4bfecba63aa98c0a43bb84b10
Griffin Hammer <grifhammer@gmail.com>
2021-03-11 13:29:05 -0800

    Fix compile warning

--- web/src/components/SetlistDisplay.tsx
+++ web/src/components/SetlistDisplay.tsx
@@ -7,10 +7,6 @@ interface ISetlistDisplayProps {
   songs: ISong[];
 }
 class SetlistDisplay extends React.Component<ISetlistDisplayProps> {
-  constructor(props: ISetlistDisplayProps) {
-    super(props);
-  }
-
   public render() {
     const songs = this.props.songs.map((song) => {
       return <p key={song.name}>{song.name}</p>;
