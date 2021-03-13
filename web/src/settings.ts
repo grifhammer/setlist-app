b3026d7b0e8d053950af253caf423189b723e413
Griffin Hammer <grifhammer@gmail.com>
2021-03-12 22:44:37 -0800

    update redirect on spotify login

--- web/src/settings.ts
+++ web/src/settings.ts
@@ -1,3 +1,3 @@
 export const clientId = "df6114c49120461ea1ec4eec20ce2334";
-export const redirectUri = "./login/failure/";
+export const redirectUri = "https://griffinhammer.com/";
 export const baseUrl = "https://tzqkyymq2k.execute-api.us-west-2.amazonaws.com";
