24ab8d204bc9bad30094efacb0255c56677ba91e
Griffin Hammer <grifhammer@gmail.com>
2021-08-07 08:49:53 -0700

    fix lockfile

--- web/src/actions/GetPopularArtists.ts
+++ web/src/actions/GetPopularArtists.ts
@@ -1,14 +1,11 @@
 export const GetPopularArtists = (page: number) => {
-  return async (dispatch) => {
+  return async (dispatch: any) => {
     try {
       dispatch({
         type: "POPULAR_ARTISTS_LOADING",
       });
 
-      const perPage = 10;
-      const offset = page * perPage - perPage;
-
-      const res = await axios.get();
+      const res = await fetch("");
       const data = res.json();
       dispatch({
         type: "POPULAR_ARTISTS_SUCCESS",
