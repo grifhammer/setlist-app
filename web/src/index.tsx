01dc025d29557035536ef4f958d825337cfbb51d
grifhammer <grifhammer@gmail.com>
2021-08-22 16:20:53 -0700

    add amplify configuration

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -6,18 +6,21 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { ConnectedRouter } from "connected-react-router";
 import configureStore, { history } from "./configureStore";
+import Amplify, { Auth } from "aws-amplify";
+import awsconfig from "../amplify/.config/project-config.json";
 const store = configureStore({});
+Amplify.configure(awsconfig);
 
 console.log(process.env);
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <ConnectedRouter history={history}>
-        <App />
-      </ConnectedRouter>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+	<React.StrictMode>
+		<Provider store={store}>
+			<ConnectedRouter history={history}>
+				<App />
+			</ConnectedRouter>
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById("root")
 );
 
 reportWebVitals();
