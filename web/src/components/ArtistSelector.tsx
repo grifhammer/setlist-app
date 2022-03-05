7d34adcc8abcde9d5c83201d2d0d04ea584b0532
Griffin Hammer <grifhammer@gmail.com>
2022-03-04 22:13:40 -0800

    fixing build

--- web/src/components/ArtistSelector.tsx
+++ web/src/components/ArtistSelector.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useEffect } from "react";
+import { FunctionComponent, useEffect } from "react";
 
 import { Artist } from "types";
 
@@ -6,59 +6,59 @@ import "./ArtistSelector.css";
 import { History, Location } from "history";
 import { useDispatch, useSelector } from "react-redux";
 import {
-  searchArtist,
-  searchSetlists,
-  ArtistState,
+	searchArtist,
+	searchSetlists,
+	ArtistState,
 } from "src/reducers/ArtistReducer";
 import { useParams } from "react-router-dom";
 import ISetlist from "src/models/Setlist";
 import SetlistDisplay from "./SetlistDisplay";
 
 interface ArtistSelectorProps {
-  history: History;
-  location: Location<{ artists: Artist[]; selectedArtist: string }>;
+	history: History;
+	location: Location<{ artists: Artist[]; selectedArtist: string }>;
 }
 
 const ArtistSelector: FunctionComponent<ArtistSelectorProps> = ({
-  ...props
+	...props
 }) => {
-  const { artist } = useParams<{ artist: string }>();
-  const dispatch = useDispatch();
-  const { artists, setlists } = useSelector<{ Artist: any }, ArtistState>(
-    (store) => {
-      console.log(store);
-      return store.Artist;
-    }
-  );
+	const { artist } = useParams<{ artist: string }>();
+	const dispatch = useDispatch();
+	const { artists, setlists } = useSelector<{ Artist: any }, ArtistState>(
+		(store) => {
+			console.log(store);
+			return store.Artist;
+		}
+	);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      await searchArtist(artist)(dispatch);
-    };
-    fetchData();
-  }, [artist, dispatch]);
-  if (!artists || artists.length <= 0) {
-    return null;
-  }
-  const artistElements = artists.map((artist) => {
-    return (
-      <div
-        className="artist-selector"
-        onClick={() => {
-          searchSetlists(artist.mbid)(dispatch);
-        }}
-        key={artist.mbid}
-      >
-        <h2 className="artist-details">
-          {artist.name} {artist.disambiguation}
-        </h2>
-        {setlists[artist.mbid].map((result: ISetlist) => {
-          return <SetlistDisplay key={result.id} setlist={result} />;
-        })}
-      </div>
-    );
-  });
-  return <form>{artistElements}</form>;
+	useEffect(() => {
+		const fetchData = async () => {
+			await searchArtist(artist)(dispatch);
+		};
+		fetchData();
+	}, [artist, dispatch]);
+	if (!artists || artists.length <= 0) {
+		return null;
+	}
+	const artistElements = artists.map((artist) => {
+		return (
+			<div
+				className="artist-selector"
+				onClick={() => {
+					searchSetlists(artist.mbid)(dispatch);
+				}}
+				key={artist.mbid}
+			>
+				<h2 className="artist-details">
+					{artist.name} {artist.disambiguation}
+				</h2>
+				{setlists[artist.mbid].map((result: ISetlist) => {
+					return <SetlistDisplay key={result.id} setlist={result} />;
+				})}
+			</div>
+		);
+	});
+	return <form>{artistElements}</form>;
 };
 
 export default ArtistSelector;
