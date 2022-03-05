2a25daa3b615d0565c0f5d4eaf1d40c743c2e65c
Griffin Hammer <grifhammer@gmail.com>
2022-03-04 22:08:59 -0800

    switch to ts configuration

--- serverless-backend/.projenrc.ts
+++ serverless-backend/.projenrc.ts
@@ -1,10 +1,16 @@
-const { awscdk } = require("projen");
+import { awscdk } from "projen";
 const project = new awscdk.AwsCdkTypeScriptApp({
 	cdkVersion: "2.1.0",
 	defaultReleaseBranch: "main",
 	name: "serverless-backend",
+	projenrcTs: true,
+	tsconfig: {
+		compilerOptions: {
+			forceConsistentCasingInFileNames: true,
+		},
+	},
 
-	deps: [] /* Runtime dependencies of this module. */,
+	// deps: [],                /* Runtime dependencies of this module. */
 	// description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
 	// devDeps: [],             /* Build dependencies for this module. */
 	// packageName: undefined,  /* The "name" in package.json. */
